570b19b03cd761b046940eab8f0c8dbe45bd80a8
humza <humza@pop-os.localdomain>
2023-05-27 01:19:10 +0100

    split n2 smart constructor into n2_left and n2_right to avoid unnecessary checks

--- lib/brolib.ml
+++ lib/brolib.ml
@@ -25,13 +25,17 @@ let n1 = function
   | N3 (t1, a1, t2, a2, t3) -> N2 (N2 (t1, a1, t2), a2, N1 t3)
   | t -> N1 t
 
-let n2 left key right =
+let n2_left left key right =
   match (left, key, right) with
   | L2 a1, a2, t1 -> N3 (N0, a1, N0, a2, t1)
   | N3 (t1, a1, t2, a2, t3), a3, N1 t4 ->
       N2 (N2 (t1, a1, t2), a2, N2 (t3, a3, t4))
   | N3 (t1, a1, t2, a2, t3), a3, (N2 _ as t4) ->
       N3 (N2 (t1, a1, t2), a2, N1 t3, a3, t4)
+  | l, k, r -> N2 (l, k, r)
+
+let n2_right left key right =
+  match (left, key, right) with
   | t1, a1, L2 a2 -> N3 (t1, a1, N0, a2, N0)
   | N1 t1, a1, N3 (t2, a2, t3, a3, t4) ->
       N2 (N2 (t1, a1, t2), a2, N2 (t3, a3, t4))
@@ -43,8 +47,8 @@ let rec ins key = function
   | N0 -> L2 key
   | N1 t -> n1 (ins key t)
   | N2 (l, k, r) as node ->
-      if key < k then n2 (ins key l) k r
-      else if key > k then n2 l k (ins key r)
+      if key < k then n2_left (ins key l) k r
+      else if key > k then n2_right l k (ins key r)
       else node
   | _ -> failwith "unexpected broset ins"
 
