e9bd55ae758fd398f790ad64ae88e6980722284e
humza <humza@pop-os.localdomain>
2023-06-05 04:14:09 +0100

    verify rope produces correct output (except for unicode differences; need to implement that)

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,11 @@
 open Brolib
 
 let rope_insert pos ins_str rope = Rope512.insert pos ins_str rope
-let rope_delete _ _ rope = rope
+let rope_delete start length rope = Rope512.delete start length rope
 let rope_to_string rope = Rope512.to_string rope
 
 let () =
-  Printf.printf "\n-\t brope edit traces \t-";
+  Printf.printf "\n-\t Rope edit traces \t-";
   let svelte =
     Utils.run_txns_time "Svelete Rope" Sveltecomponent.data Rope512.empty
       rope_insert rope_delete
@@ -22,7 +22,7 @@ let () =
     Utils.run_txns_time "Automerge Rope" Automerge.data Rope512.empty
       rope_insert rope_delete
   in
-  Printf.printf "\n-\t brope to_string \t-";
+  Printf.printf "\n-\t Rope to_string \t-";
   let _ =
     Utils.run_to_string_time "Svelte to_string Rope" svelte rope_to_string
   in
