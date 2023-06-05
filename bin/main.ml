599d9e68852900c6a8fcddf365d42075c972a02c
humza <humza@pop-os.localdomain>
2023-06-05 03:25:13 +0100

    verify substring implementation

--- bin/main.ml
+++ bin/main.ml
@@ -10,30 +10,25 @@ let () =
     Utils.run_txns_time "Svelete Rope" Sveltecomponent.data Rope512.empty
       rope_insert rope_delete
   in
-  (* let rust = *)
-  (*   Utils.run_txns_time "Rustcode Rope" Rustcode.data Rope512.empty rope_insert *)
-  (*     rope_delete *)
-  (* in *)
-  (* let seph = *)
-  (*   Utils.run_txns_time "Sephblog Rope" Sephblog.data Rope512.empty rope_insert *)
-  (*     rope_delete *)
-  (* in *)
-  (* let automerge = *)
-  (*   Utils.run_txns_time "Automerge Rope" Automerge.data Rope512.empty *)
-  (*     rope_insert rope_delete *)
-  (* in *)
+  let rust =
+    Utils.run_txns_time "Rustcode Rope" Rustcode.data Rope512.empty rope_insert
+      rope_delete
+  in
+  let seph =
+    Utils.run_txns_time "Sephblog Rope" Sephblog.data Rope512.empty rope_insert
+      rope_delete
+  in
+  let automerge =
+    Utils.run_txns_time "Automerge Rope" Automerge.data Rope512.empty
+      rope_insert rope_delete
+  in
   Printf.printf "\n-\t brope to_string \t-";
-  let svelte_string =
+  let _ =
     Utils.run_to_string_time "Svelte to_string Rope" svelte rope_to_string
   in
-  (* let _ = Utils.run_to_string_time "Rust to_string Rope" rust rope_to_string in *)
-  (* let _ = Utils.run_to_string_time "Seph to_string Rope" seph rope_to_string in *)
-  (* let _ = *)
-  (*   Utils.run_to_string_time "Automerge to_string Rope" automerge rope_to_string *)
-  (* in *)
-  let oc = open_out "svelte.txt" in
-  let out_buffer = Buffer.create (String.length svelte_string) in
-  let _ = Buffer.add_string out_buffer svelte_string in
-  let _ = Buffer.output_buffer oc out_buffer in
-  let _ = close_out oc in
+  let _ = Utils.run_to_string_time "Rust to_string Rope" rust rope_to_string in
+  let _ = Utils.run_to_string_time "Seph to_string Rope" seph rope_to_string in
+  let _ =
+    Utils.run_to_string_time "Automerge to_string Rope" automerge rope_to_string
+  in
   ()
